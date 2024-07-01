ce6b211b3d18c3c71ae033be2ed813927a0b4540
Rohit Kolekar <rohitkolekar0108@gmail.com>
2024-07-01 10:20:59 +0530

    Add project files.

---
+++ RazorCRUD/Model/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace RazorCRUD.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
